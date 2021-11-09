612081c4db49afdebd7c73ee28b0a780b378f2ad
KelvinLemanta <85560626+KelvinLemanta@users.noreply.github.com>
2021-11-09 17:47:25 +0300

    Add files via upload

---
+++ strathmore vision.cpp
@@ -0,0 +1,11 @@
+// Strathmore vission.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout<< "To become a leading out- come driven entrepreneurial research University by translating our excellence into major contribution to culture, economic well-being and quality of life. ";
+	system ("pause");
+	return 0;
+}
